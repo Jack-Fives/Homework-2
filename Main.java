009ab34fd0449d02c60b39fb6baf4523dbe1e1a9
Jack-Fives <jackf58@nycstudents.net>
2024-09-10 22:55:45 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int var_1 = 2;
+        double var_2 = 2.2;
+        boolean var_3 = true;
+        String var_4 = " Var 4";
+        double var_5 = var_1 + var_2;
+        var_3 = false;
+        final String var_final = " Done";
+        System.out.print(var_1+" "+var_2+" "+var_3+var_4+" "+var_5+" "+var_final); 
+    }
+}
